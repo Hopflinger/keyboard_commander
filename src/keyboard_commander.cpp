037fdd954a2a68df897d6a4f714f96071cc18541
Hopflinger <flinhoepflinger@gmail.com>
2024-01-10 15:50:26 +0900

    changed to keyboard.h to allow updating window. fixedarm error msg, q abs

--- src/keyboard_commander.cpp
+++ src/keyboard_commander.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
+#include "keyboard.h"
 
 #include <map>
 
@@ -30,8 +31,8 @@ Airbase Remote Control
 
 g: Set current position as desired start point
 
-Arm:  A    (Check that q_des correct before)
-Disarm:  a or A
+Arm:  P    (Check that q_des correct before)
+Disarm:  p or P
 
 Joint 1 increment:
    a    d    
@@ -95,6 +96,7 @@ int main(int argc, char** argv)
   // Init ROS node
   ros::init(argc, argv, "keyboard_commander");
   ros::NodeHandle nh;
+  init_keyboard();
 
   ros::Subscriber jointAngle_sub = nh.subscribe("joint_angles", 100, &jointAngleCallback);
 
@@ -108,33 +110,40 @@ int main(int argc, char** argv)
   geometry_msgs::Vector3 velocity;
   std_msgs::Bool arm_msg;  
 
-
+  ros::Rate rate(500); 
   while(true){
 
-    // Get the pressed key
-    key = getch();
+    // Get the pressed key non blcking with kbhit
+    if( _kbhit()){
+      key = _getch();
+    }
 
     //if it corresponds to a key
-    if (key == 'A' && !armed){
+    if (key == 'P' && !armed){
 
-      if ( abs(q1_des-q[0]) > 4.0 || abs(q2_des-q[1]) > 4.0  )
+      if ( abs(q1_des-q[0]) > 2.0 || abs(q2_des-q[1]) > 2.0  )
       {
         error_warn = true;
       }
       else{
+        error_warn = false;
         armed = true;      
         arm_msg.data = armed;
         arm_pub.publish(arm_msg);
+        rel_q_des[0] = 0;
+        rel_q_des[1] = 0;
         printf("\nArming____________________________________");
       }
 
       key = ' ';
     }
     
-    if ((key == 'a' || key == 'A') && armed){
+    if ((key == 'p' || key == 'P') && armed){
       armed = false;
       arm_msg.data = armed;
       arm_pub.publish(arm_msg);
+      rel_q_des[0] = 0;
+      rel_q_des[1] = 0;
 
       printf("\nDisarming____________________________________");
       key = ' ';
@@ -152,12 +161,12 @@ int main(int argc, char** argv)
     if (speedBindings.count(key) == 1  && armed)
     {
       // Grab the pos data
-      q1_des = q1_des_0 + q1_des + speedBindings[key][0];
-      q2_des = q2_des_0 + q2_des + speedBindings[key][1];
+      rel_q_des[0] = rel_q_des[0] + speedBindings[key][0];
+      rel_q_des[1] = rel_q_des[1] + speedBindings[key][1];
       p  = p  + speedBindings[key][2];
+      key = ' ';
     }
 
-    else{
       // If ctrl-C (^C) was pressed, terminate the program
       if (key == '\x03')
       {
@@ -165,8 +174,7 @@ int main(int argc, char** argv)
         printf("\n\n                 .     .\n              .  |\\-^-/|  .    \n             /| } O.=.O { |\\\n\n                 CH3EERS\n\n");
         break;
       }
-      printf("\nInvalid command! ");
-    }
+
 
     q1_des = q1_des_0 + rel_q_des[0];
     q2_des = q2_des_0 + rel_q_des[1];
@@ -182,7 +190,7 @@ int main(int argc, char** argv)
     }
     printf("\nCurrent:   q1: %.2f\t q2: %.2f ", q[0], q[1]); 
     printf("\nDesired:   q1: %.2f\t q2: %.2f ", q1_des, q2_des);    
-    printf("\nRel.Des.:  q1: %.2f\t q2: %.2f \t| Last command: %c   ", rel_q_des[0], rel_q_des[1], key);
+    printf("\nRel.Des.: dq1: %.2f\tdq2: %.2f \t| Last command: %c   ", rel_q_des[0], rel_q_des[1], key);
 
     if (error_warn){
       printf("\nWarning: Large Delta between Set and Current Position: Arming Disabled");
@@ -203,7 +211,8 @@ int main(int argc, char** argv)
     pub.publish(point);
     velocity_pub.publish(velocity);
     ros::spinOnce();
+    rate.sleep();
   }
-
+  close_keyboard();
   return 0;
 }
